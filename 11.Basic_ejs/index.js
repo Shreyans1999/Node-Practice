4f5b0bfdb780f2d21b6f400bf909c14ddcf6fe51
shreyans <ssaklecha@primesoft.net>
2024-04-05 23:14:14 +0530

    added ejs

--- 11.Basic_ejs/index.js
+++ 11.Basic_ejs/index.js
@@ -2,30 +2,22 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const publicPath=path.join(__dirname,'public')
+const publicPath = path.join(__dirname, 'public');
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs')
 
-app.get('',(_,resp)=>{
-    resp.sendFile(`${publicPath}/index.html`)
-});
+app.get('',(req,resp)=>{
+    resp.sendFile(`${publicPath}/index.html`);
+})
 
-app.get('/profile',(_,resp)=>{
-    const user={
-        name:'Peter',
-        email:'peter@test.com',
-        country:'USA'
+app.get('/profile',(req,resp)=>{
+    const user = {
+        name:"shreyans",
+        email:"shreyans@test.com",
+        city:"ajmer"
     }
-   resp.render('profile',{user})
-});
-
-app.get('/contact',(_,resp)=>{
-    resp.sendFile(`${publicPath}/about.html`)
-});
-
-app.get('/help',(_,resp)=>{
-    resp.sendFile(`${publicPath}/help.html`)
-});
+    resp.render(`profile`,{user});
+})
 
 app.get('*',(_,resp)=>{
     resp.sendFile(`${publicPath}/nopage.html`)
