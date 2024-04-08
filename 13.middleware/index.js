7cb5bf48f70b8c4f98009c3575cb105be0b536f6
shreyans <ssaklecha@primesoft.net>
2024-04-08 15:16:37 +0530

    added middleware

---
+++ 13.middleware/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+const reqFilter = (req,resp,next)=>{
+    if(!req.query.age){
+        resp.send('Please provide age!!');
+    }
+    else if(req.query.age<18){
+        resp.send('Age should be greater than 18')
+    }
+    else{
+        next();
+    }
+}
+
+app.use(reqFilter);
+
+app.get('',(req,resp)=>{
+    resp.send('Welcome to home page');
+})
+
+app.listen(5000);
